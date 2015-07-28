8f00b462da3380675a22dc2386f98bbfe8ef57d8
sujal99 <sujal.ghosh@gmail.com>
2015-07-28 20:53:39 +0530

    data have been added.

--- js/helper.js
+++ js/helper.js
@@ -62,11 +62,11 @@ var googleMap = '<div id="map"></div>';
 /*
 The International Name challenge in Lesson 2 where you'll create a function that will need this helper code to run. Don't delete! It hooks up your code to the button you'll be appending.
 */
-$(document).ready(function() {
-  $('button').click(function() {
-    var iName = inName() || function(){};
-    $('#name').html(iName);  
-  });
+$(document).ready(function () {
+	$('button').click(function () {
+		var iName = inName() || function () {};
+		$('#name').html(iName);
+	});
 });
 
 /*
@@ -74,18 +74,16 @@ The next few lines about clicks are for the Collecting Click Locations quiz in L
 */
 clickLocations = [];
 
-function logClicks(x,y) {
-  clickLocations.push(
-    {
-      x: x,
-      y: y
-    }
-  );
-  console.log('x location: ' + x + '; y location: ' + y);
+function logClicks(x, y) {
+	clickLocations.push({
+		x: x,
+		y: y
+	});
+	console.log('x location: ' + x + '; y location: ' + y);
 }
 
-$(document).click(function(loc) {
-  // your code goes here!
+$(document).click(function (loc) {
+	// your code goes here!
 });
 
 
@@ -103,127 +101,127 @@ Start here! initializeMap() is called when page is loaded.
 */
 function initializeMap() {
 
-  var locations;
+	var locations;
 
-  var mapOptions = {
-    disableDefaultUI: true
-  };
-
-
-  map = new google.maps.Map(document.querySelector('#map-div'), mapOptions);
+	var mapOptions = {
+		disableDefaultUI: true
+	};
 
 
-  /*
-  locationFinder() returns an array of every location string from the JSONs
-  written for bio, education, and work.
-  */
-  function locationFinder() {
+	map = new google.maps.Map(document.querySelector('#map'), mapOptions);
 
-    // initializes an empty array
-    var locations = [];
 
-    // adds the single location property from bio to the locations array
-    locations.push(bio.contacts.location);
+	/*
+	locationFinder() returns an array of every location string from the JSONs
+	written for bio, education, and work.
+	*/
+	function locationFinder() {
 
-    // iterates through school locations and appends each location to
-    // the locations array
-    for (var school in education.schools) {
-      locations.push(education.schools[school].location);
-    }
+		// initializes an empty array
+		var locations = [];
 
-    // iterates through work locations and appends each location to
-    // the locations array
-    for (var job in work.jobs) {
-      locations.push(work.jobs[job].location);
-    }
+		// adds the single location property from bio to the locations array
+		locations.push(bio.contacts.location);
 
-    return locations;
-  }
+		// iterates through school locations and appends each location to
+		// the locations array
+		for (var school in education.schools) {
+			locations.push(education.schools[school].location);
+		}
 
-  /*
-  createMapMarker(placeData) reads Google Places search results to create map pins.
-  placeData is the object returned from search results containing information
-  about a single location.
-  */
-  function createMapMarker(placeData) {
+		// iterates through work locations and appends each location to
+		// the locations array
+		for (var job in work.jobs) {
+			locations.push(work.jobs[job].location);
+		}
 
-    // The next lines save location data from the search result object to local variables
-    var lat = placeData.geometry.location.lat();  // latitude from the place service
-    var lon = placeData.geometry.location.lng();  // longitude from the place service
-    var name = placeData.formatted_address;   // name of the place from the place service
-    var bounds = window.mapBounds;            // current boundaries of the map window
+		return locations;
+	}
 
-    // marker is an object with additional data about the pin for a single location
-    var marker = new google.maps.Marker({
-      map: map,
-      position: placeData.geometry.location,
-      title: name
-    });
+	/*
+	createMapMarker(placeData) reads Google Places search results to create map pins.
+	placeData is the object returned from search results containing information
+	about a single location.
+	*/
+	function createMapMarker(placeData) {
 
-    // infoWindows are the little helper windows that open when you click
-    // or hover over a pin on a map. They usually contain more information
-    // about a location.
-    var infoWindow = new google.maps.InfoWindow({
-      content: name
-    });
+		// The next lines save location data from the search result object to local variables
+		var lat = placeData.geometry.location.lat(); // latitude from the place service
+		var lon = placeData.geometry.location.lng(); // longitude from the place service
+		var name = placeData.formatted_address; // name of the place from the place service
+		var bounds = window.mapBounds; // current boundaries of the map window
 
-    // hmmmm, I wonder what this is about...
-    google.maps.event.addListener(marker, 'click', function() {
-      // your code goes here!
-    });
+		// marker is an object with additional data about the pin for a single location
+		var marker = new google.maps.Marker({
+			map: map,
+			position: placeData.geometry.location,
+			title: name
+		});
 
-    // this is where the pin actually gets added to the map.
-    // bounds.extend() takes in a map location object
-    bounds.extend(new google.maps.LatLng(lat, lon));
-    // fit the map to the new marker
-    map.fitBounds(bounds);
-    // center the map
-    map.setCenter(bounds.getCenter());
-  }
+		// infoWindows are the little helper windows that open when you click
+		// or hover over a pin on a map. They usually contain more information
+		// about a location.
+		var infoWindow = new google.maps.InfoWindow({
+			content: name
+		});
 
-  /*
-  callback(results, status) makes sure the search returned results for a location.
-  If so, it creates a new map marker for that location.
-  */
-  function callback(results, status) {
-    if (status == google.maps.places.PlacesServiceStatus.OK) {
-      createMapMarker(results[0]);
-    }
-  }
+		// hmmmm, I wonder what this is about...
+		google.maps.event.addListener(marker, 'click', function () {
+			infoWindow.open(map, marker);
+		});
 
-  /*
-  pinPoster(locations) takes in the array of locations created by locationFinder()
-  and fires off Google place searches for each location
-  */
-  function pinPoster(locations) {
+		// this is where the pin actually gets added to the map.
+		// bounds.extend() takes in a map location object
+		bounds.extend(new google.maps.LatLng(lat, lon));
+		// fit the map to the new marker
+		map.fitBounds(bounds);
+		// center the map
+		map.setCenter(bounds.getCenter());
+	}
 
-    // creates a Google place search service object. PlacesService does the work of
-    // actually searching for location data.
-    var service = new google.maps.places.PlacesService(map);
+	/*
+	callback(results, status) makes sure the search returned results for a location.
+	If so, it creates a new map marker for that location.
+	*/
+	function callback(results, status) {
+		if (status == google.maps.places.PlacesServiceStatus.OK) {
+			createMapMarker(results[0]);
+		}
+	}
 
-    // Iterates through the array of locations, creates a search object for each location
-    for (var place in locations) {
+	/*
+	pinPoster(locations) takes in the array of locations created by locationFinder()
+	and fires off Google place searches for each location
+	*/
+	function pinPoster(locations) {
 
-      // the search request object
-      var request = {
-        query: locations[place]
-      };
+		// creates a Google place search service object. PlacesService does the work of
+		// actually searching for location data.
+		var service = new google.maps.places.PlacesService(map);
 
-      // Actually searches the Google Maps API for location data and runs the callback
-      // function with the search results after each search.
-      service.textSearch(request, callback);
-    }
-  }
+		// Iterates through the array of locations, creates a search object for each location
+		for (var place in locations) {
 
-  // Sets the boundaries of the map based on pin locations
-  window.mapBounds = new google.maps.LatLngBounds();
+			// the search request object
+			var request = {
+				query: locations[place]
+			};
 
-  // locations is an array of location strings returned from locationFinder()
-  locations = locationFinder();
+			// Actually searches the Google Maps API for location data and runs the callback
+			// function with the search results after each search.
+			service.textSearch(request, callback);
+		}
+	}
 
-  // pinPoster(locations) creates pins on the map for each location in
-  // the locations array
-  pinPoster(locations);
+	// Sets the boundaries of the map based on pin locations
+	window.mapBounds = new google.maps.LatLngBounds();
+
+	// locations is an array of location strings returned from locationFinder()
+	locations = locationFinder();
+
+	// pinPoster(locations) creates pins on the map for each location in
+	// the locations array
+	pinPoster(locations);
 
 }
 
@@ -232,11 +230,11 @@ Uncomment the code below when you're ready to implement a Google Map!
 */
 
 // Calls the initializeMap() function when the page loads
-//window.addEventListener('load', initializeMap);
+window.addEventListener('load', initializeMap);
 
 // Vanilla JS way to listen for resizing of the window
 // and adjust map bounds
-//window.addEventListener('resize', function(e) {
-  //Make sure the map bounds get updated on page resize
-//  map.fitBounds(mapBounds);
-//});
+window.addEventListener('resize', function (e) {
+	//Make sure the map bounds get updated on page resize
+	map.fitBounds(mapBounds);
+});
--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,195 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+
+/*jslint browser: true*/
+/*global $, HTMLheaderName, HTMLheaderRole, HTMLskills,
+HTMLskillsStart, HTMLworkEmployer*/
+
+
+var bio = {
+	"name": "Sujal Ghosh",
+	"role": "Application Developer",
+	"contacts": {
+		"mobile": "+919830851589",
+		"email": "sujal.ghosh@gmail.com",
+		"github": "sujal99",
+		"twitter": "@sujal_ghosh",
+		"location": "Calcutta, India"
+	},
+	"welcomeMessage": "I am a application developer with 6 years of experiance. I have significant experiance in developing iOS and web apps.",
+	"bioPic": "images/fry.jpg",
+	"skills": ["HTML5", "CSS3", "JavaScript", "iOS"]
+};
+
+var education = {
+	"schools": [
+		{
+			"name": "Bengal Engineering and Science University",
+			"location": "India",
+			"degree": "MCA",
+			"majors": ["Computer Science"],
+			"dates": "2003-2008",
+			"url": "http://www.iiests.ac.in/"
+		},
+		{
+			"name": "University of Calcutta",
+			"location": "India",
+			"degree": "B.Sc.",
+			"majors": ["Mathematics"],
+			"dates": "1999-2003",
+			"url": "http://www.caluniv.ac.in/"
+		}
+	],
+	"onlineCourses": [
+		{
+			"title": "Principles of Computing",
+			"school": "Coursera",
+			"dates": "2014",
+			"url": "https://www.coursera.org/"
+		},
+		{
+			"title": "Algorithm",
+			"school": "EdX",
+			"dates": "2014",
+			"url": "https://www.edx.org/"
+		}
+	]
+};
+
+var work = {
+	"jobs": [
+		{
+			"employer": "Adept Software Avenues Pvt. Ltd.",
+			"title": "Software Developer",
+			"location": "Calcutta, India",
+			"dates": "2008-2015",
+			"description": "C++ developer"
+		}
+	]
+};
+
+var projects = {
+	"projects": [
+		{
+			"title": "Sample Project 1",
+			"dates": "2014",
+			"description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse vehicula placerat luctus. Duis at sapien enim. Duis lobortis urna eu felis vestibulum, ac suscipit augue commodo. Duis sit amet urna luctus, vehicula enim a, egestas tellus. Nullam est risus, laoreet quis nibh et, vehicula feugiat mi. Nam lacus odio, sodales sed luctus id, varius mattis diam. Pellentesque dictum rhoncus eros a rutrum. Nulla a rhoncus orci, vel ultricies lectus. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Cras eu ipsum elementum, rutrum elit vehicula, scelerisque justo. Nam at tortor elit. Vestibulum vehicula pharetra purus. Pellentesque dapibus orci ut velit condimentum, ac sagittis nisl dignissim.",
+			"images": [
+				"https://pbs.twimg.com/profile_images/1130950496/agnes_despicable_me.png",
+			"http://www11.imagesandwallpapers.com/img/flipped_Cute_Despicable_Me_Agnes_HD_Wallpaper.jpg"
+			]
+		},
+		{
+			"title": "Sample Project 2",
+			"dates": "2015",
+			"description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse vehicula placerat luctus. Duis at sapien enim. Duis lobortis urna eu felis vestibulum, ac suscipit augue commodo. Duis sit amet urna luctus, vehicula enim a, egestas tellus. Nullam est risus, laoreet quis nibh et, vehicula feugiat mi. Nam lacus odio, sodales sed luctus id, varius mattis diam. Pellentesque dictum rhoncus eros a rutrum. Nulla a rhoncus orci, vel ultricies lectus. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Cras eu ipsum elementum, rutrum elit vehicula, scelerisque justo. Nam at tortor elit. Vestibulum vehicula pharetra purus. Pellentesque dapibus orci ut velit condimentum, ac sagittis nisl dignissim.",
+			"images": [
+				"https://pbs.twimg.com/profile_images/1130950496/agnes_despicable_me.png",
+			"http://www11.imagesandwallpapers.com/img/flipped_Cute_Despicable_Me_Agnes_HD_Wallpaper.jpg"
+			]
+		}
+	]
+};
+
+
+function displayHeader() {
+	var formattedName = HTMLheaderName.replace("%data%", bio.name);
+	var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
+	$("#header").prepend(formattedRole);
+	$("#header").prepend(formattedName);
+}
+
+function displayWorks() {
+	if (bio.skills.length > 0) {
+		$("#header").append(HTMLskillsStart);
+		bio.skills.forEach(function (currentValue, index, array) {
+			var formattedSkill = HTMLskills.replace("%data%", currentValue);
+			$("#skills").append(formattedSkill);
+		});
+	}
+
+	work.jobs.forEach(function (currentValue, index, array) {
+		$("#workExperience").append(HTMLworkStart);
+		var formattedEmployerTitle = HTMLworkEmployer.replace("%data%", currentValue.employer)
+			+ HTMLworkLocation.replace("%data%", currentValue.location)
+			+ HTMLworkTitle.replace("%data%", currentValue.title)
+			+ HTMLworkDates.replace("%data%", currentValue.dates)
+			+ HTMLworkDescription.replace("%data%", currentValue.description);
+		$(".work-entry:last").append(formattedEmployerTitle);
+	});
+}
+
+function displayContacts(contactList) {
+	for (var property in bio.contacts) {
+		var listItem = HTMLcontactGeneric.replace("%contact%", property);
+		listItem = listItem.replace("%data%", bio.contacts[property]);
+		contactList.append(listItem);
+	}
+}
+
+function displayImageAndWelcome() {
+	$("#header").append(HTMLbioPic.replace("%data%", bio.bioPic));
+	$("#header").append(HTMLwelcomeMsg.replace("%data%", bio.welcomeMessage));
+}
+
+function displayProjects() {
+	projects.projects.forEach(function (currentVal, index, array) {
+		$("#projects").append(HTMLprojectStart);
+		$(".project-entry:last").append(HTMLprojectTitle.replace("%data%", currentVal.title));
+		$(".project-entry:last").append(HTMLprojectDates.replace("%data%", currentVal.dates));
+		$(".project-entry:last").append(HTMLprojectDescription.replace("%data%", currentVal.description));
+		currentVal.images.forEach(function (curVal, index1, array1) {
+			$(".project-entry:last").append(HTMLprojectImage.replace("%data%", curVal));
+		});
+	});
+}
+
+function displayEducation() {
+	education.schools.forEach(function (currentVal, index, array){
+		$("#education").append(HTMLschoolStart);
+		var forMattedSchoolName = HTMLschoolName.replace("%data%", currentVal.name)
+			+ HTMLschoolDegree.replace("%data%", currentVal.degree);
+ 		$(".education-entry:last").append(forMattedSchoolName);
+		$(".education-entry:last").append(HTMLschoolLocation.replace("%data%", currentVal.location));
+		$(".education-entry:last").append(HTMLschoolDates.replace("%data%", currentVal.dates));
+		$(".education-entry:last").append(HTMLschoolMajor.replace("%data%", currentVal.majors));
+	});
+
+	if (education.onlineCourses.length > 0) {
+		$("#education").append(HTMLonlineClasses);
+		education.onlineCourses.forEach(function (currentVal, index, array) {
+            $("#education").append(HTMLschoolStart);
+            var formattedCourseName = HTMLonlineTitle.replace("%data%", currentVal.title)
+			+ HTMLonlineSchool.replace("%data%", currentVal.school);
+            $(".education-entry:last").append(formattedCourseName);
+            $(".education-entry:last").append(HTMLonlineDates.replace("%data%", currentVal.dates));
+            $(".education-entry:last").append(HTMLonlineURL.replace("%data%", currentVal.url));
+        });
+	}
+}
+
+function locationDisplay() {
+	//$("#map-div").append(googleMap);
+}
+
+
+displayHeader();
+displayContacts($("#topContacts"));
+displayImageAndWelcome();
+displayWorks();
+displayProjects();
+displayEducation();
+locationDisplay();
+displayContacts($("#footerContacts"));
+
+//$(document).click(function (loc) {
+//	logClicks(loc.pageX, loc.pageY);
+//});
+//
+//function inName(name1) {
+//	var names = name1.split(/\s+/);
+//	return names[0].charAt(0).toUpperCase + names[0].slice(1) + " " + names[1].toUpperCase;
+//}
+//
+//$("#main").append(internationalizeButton);
